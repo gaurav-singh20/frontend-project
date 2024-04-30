205136e27f792557580d19f5e33a0484ea89b08d
Gaurav Singh <120627440+gaurav-singh20@users.noreply.github.com>
2024-05-01 04:41:11 +0530

    removed old

--- header.js
+++
@@ -1,8 +0,0 @@
-// header.js
-window.addEventListener('DOMContentLoaded', function() {
-  fetch('header.html')
-      .then(response => response.text())
-      .then(data => {
-          document.getElementById('header').innerHTML = data;
-      });
-});
